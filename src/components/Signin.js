507e8d5134c57952fd5b0b8cad3450dc3d7d0cf5
Joseph Wangemann <fractalscape13@gmail.com>
2020-05-06 17:26:11 -0700

    use conditional to show login/signup OR logout

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,10 +1,12 @@
 import React from "react";
 import firebase from 'firebase/app';
-// import { withFirestore, isLoaded } from 'react-redux-firebase';
+import { withFirestore, isLoaded } from 'react-redux-firebase';
 import 'firebase/auth';
 
 
-function Signin(){
+function Signin(props){
+
+  console.log('props', props)
   
   function doSignUp(event) {
     event.preventDefault();
@@ -22,6 +24,7 @@ function Signin(){
     const email = event.target.signinEmail.value;
     const password = event.target.signinPassword.value;
     firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+      console.log(props.firebase)
       console.log("Successfully logged IN!");
     }).catch(function(error) {
       console.log(error.message);
@@ -35,86 +38,88 @@ function Signin(){
       console.log(error.message);
     });
   }
-  
-  //attempting to do a conditional rendering here similar to the rendering in MemoryControl.js so the signup/signin/signout forms only show at the correct time.  not functional currently, can't figure out how to access "auth" like i'm doing in MemoryControl.js
 
-  // const auth = this.props.firebase.auth();
-  // if (!isLoaded(auth)) {
-  //   return (
-  //     <div className="signin">
-  //       <form onSubmit={doSignUp}>
-  //         <input
-  //           type='text'
-  //           name='email'
-  //           placeholder='Email address' />
-  //         <input
-  //           type='password'
-  //           name='password'
-  //           placeholder='Password' />
-  //         <button type='submit'>Sign up</button>
-  //       </form>
-  //     </div>
-  //   )
-  // }
-  // if ((isLoaded(auth)) && (auth.currentUser == null)) {
-  //   return (
-  //     <div className="signin">
-  //       <h1>Sign In</h1>
-  //       <form onSubmit={doSignIn}>
-  //         <input
-  //           type='text'
-  //           name='signinEmail'
-  //           placeholder='Email address' />
-  //         <input
-  //           type='password'
-  //           name='signinPassword'
-  //           placeholder='Password' />
-  //         <button type='submit'>Log in</button>
-  //       </form>
-  //     </div>
-  //   )
-  // }
-  // if ((isLoaded(auth)) && (auth.currentUser != null)) {
-  //   return (
-  //     <div className="signin">
-  //       <h1>Sign Out</h1>
-  //       <button onClick={doSignOut}>Sign out</button>
-  //     </div>
-  //   )
-  // } 
+  let auth = props.firebase.auth();
+  console.log("auth", auth)
+  if (!isLoaded(auth)) {
+    return (
+      <div className="signin">
+        
+      </div>
+    )
+  }
+  if ((isLoaded(auth)) && (auth.currentUser == null)) {
+    return (
+      <div className="signin">
+        <h1>Sign In</h1>
+        <form onSubmit={doSignIn}>
+          <input
+            type='text'
+            name='signinEmail'
+            placeholder='Email address' />
+          <input
+            type='password'
+            name='signinPassword'
+            placeholder='Password' />
+          <button type='submit'>Log in</button>
+        </form>
+        <h1>Sign UP!</h1>
+        <form onSubmit={doSignUp}>
+          <input
+            type='text'
+            name='email'
+            placeholder='Email address' />
+          <input
+            type='password'
+            name='password'
+            placeholder='Password' />
+          <button type='submit'>Sign up</button>
+        </form>
+      </div>
+    )
+  }
+  if ((isLoaded(auth)) && (auth.currentUser != null)) {
+    console.log("we made it")
+    return (
+      <div className="signin">
+        <h1>Sign Out</h1>
+        <button onClick={doSignOut}>Sign out</button>
+      </div>
+    )
+  } 
 
-  return (
-    <div className="signin">
-      <h1>Sign UP</h1>
-      <form onSubmit={doSignUp}>
-        <input
-          type='text'
-          name='email'
-          placeholder='Email address' />
-        <input
-          type='password'
-          name='password'
-          placeholder='Password' />
-        <button type='submit'>Sign up</button>
-      </form>
+  // return (
+  //   <div className="signin">
+  //     <h1>Sign UP</h1>
+  //     <form onSubmit={doSignUp}>
+  //       <input
+  //         type='text'
+  //         name='email'
+  //         placeholder='Email address' />
+  //       <input
+  //         type='password'
+  //         name='password'
+  //         placeholder='Password' />
+  //       <button type='submit'>Sign up</button>
+  //     </form>
 
-      <h1>Sign IN</h1>
-      <form onSubmit={doSignIn}>
-        <input
-          type='text'
-          name='signinEmail'
-          placeholder='Email address' />
-        <input
-          type='password'
-          name='signinPassword'
-          placeholder='Password' />
-        <button type='submit'>Log in</button>
-      </form>
+  //     <h1>Sign IN</h1>
+  //     <form onSubmit={doSignIn}>
+  //       <input
+  //         type='text'
+  //         name='signinEmail'
+  //         placeholder='Email address' />
+  //       <input
+  //         type='password'
+  //         name='signinPassword'
+  //         placeholder='Password' />
+  //       <button type='submit'>Log in</button>
+  //     </form>
 
-      <h1>Sign OUT</h1>
-      <button onClick={doSignOut}>Sign out</button>
-    </div>
-  );
+  //     <h1>Sign OUT</h1>
+  //     <button onClick={doSignOut}>Sign out</button>
+  //   </div>
+  // );
 }
 
-export default Signin;
+export default withFirestore(Signin);
